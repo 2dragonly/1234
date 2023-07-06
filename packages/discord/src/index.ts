bd0c62bd27384d28c59fac23e399da418b73d200
2dragonly y <2dragonly@proton.me>
2023-07-06 16:44:38 +0000

    first commit

---
+++ packages/discord/src/index.ts
@@ -0,0 +1,17 @@
+import "dotenv/config";
+
+import { initialize } from "./commands/chat/poe";
+import Client from "./structures/client";
+
+process.on("uncaughtException", (error) => {
+	console.error(`Uncaught Exception: ${error.message}`);
+});
+process.on("unhandledRejection", (error) => {
+	if (error instanceof Error) console.error(`Unhandled Rejection: ${error.message}`);
+});
+
+(async () => {
+	await initialize();
+	const client = new Client();
+	client.start();
+})();
